bc4297ca824482f9f5fba588c0da79c4accb4fe4
juddB <Juddbahoui@gmail.com>
2021-02-24 13:32:30 -0500

    Done.

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -67,13 +67,52 @@ class MusicLibraryController
         songs_sorted_by_genre = Genre.all.sort_by do |genre|
             genre.name
           end
-        binding.pry
+    
           songs_sorted_by_genre.each.with_index(1) do |genre,index|
             
             puts "#{index}. #{genre.name}"
           end
         end
 
+        def list_songs_by_artist
+            puts "Please enter the name of an artist:"
+            input = gets.chomp
+            if artist = Artist.find_by_name(input) #find artist that matches input
+              songs_sorted_by_name = artist.songs.sort_by do |song|
+                song.name
+              end
+              songs_sorted_by_name.each.with_index(1) do |song,index|
+                puts "#{index}. #{song.name} - #{song.genre.name}"
+              end
+            end
+          end
+        
+          def list_songs_by_genre
+            puts "Please enter the name of a genre:"
+            input = gets.chomp
+            if genre = Genre.find_by_name(input) #find genre that matches input
+              songs_sorted_by_name = genre.songs.sort_by do |song|
+                song.name
+              end
+              songs_sorted_by_name.each.with_index(1) do |song,index|
+                puts "#{index}. #{song.artist.name} - #{song.name}"
+              end
+            end
+          end
+        
+          def play_song
+            puts "Which song number would you like to play?"
+            list_of_songs =  Song.all.sort{ |a, b| a.name <=> b.name }
+      
+            input = gets.strip.to_i
+            if (1..Song.all.length).include?(input)
+              song = list_of_songs[input+2]
+            #   binding.pry
+              puts "Playing #{song.name} by #{song.artist.name}"
+              
+            end
+          end
+  
     
     
 
